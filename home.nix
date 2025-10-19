14bbb68d7598b2d730fcddebbae05ee74cd01a5e
Juan C Nuno <juancnuno@gmail.com>
2025-10-19 16:42:54 -0700

    Configure git

--- home.nix
+++ home.nix
@@ -80,4 +80,41 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.git.enable = true;
+
+  programs.git.settings = {
+    credential = {
+      "https://gist.github.com" = {
+        helper = [
+          ""
+          "!/run/current-system/sw/bin/gh auth git-credential"
+        ];
+      };
+
+      "https://github.com" = {
+        helper = [
+          ""
+          "!/run/current-system/sw/bin/gh auth git-credential"
+        ];
+      };
+    };
+
+    diff = {
+      tool = "meld";
+    };
+
+    difftool = {
+      prompt = false;
+    };
+
+    init = {
+      defaultBranch = "main";
+    };
+
+    user = {
+      email = "juancnuno@gmail.com";
+      name = "Juan C Nuno";
+    };
+  };
 }

839f43d797d35dfac0b380c3c1840bf73e991f6a
Juan C Nuno <juancnuno@gmail.com>
2025-10-19 13:16:04 -0700

    Install home-manager

--- configuration.nix
+++ configuration.nix
@@ -107,9 +107,9 @@
     gh
     git
     google-chrome
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    home-manager
+    vim
     vscode
-  #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

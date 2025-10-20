c26f537ea34e2962c58e1786b2b6142cd0b2f0d3
Juan C Nuno <juancnuno@gmail.com>
2025-10-19 18:02:28 -0700

    Configure Visual Studio Code

--- home.nix
+++ home.nix
@@ -117,4 +117,19 @@
       name = "Juan C Nuno";
     };
   };
+
+  programs.vscode.enable = true;
+
+  programs.vscode.profiles.default.userSettings = {
+    "editor.cursorBlinking" = "solid";
+
+    "editor.rulers" = [
+      100
+    ];
+
+    "files.trimTrailingWhitespace" = true;
+    "window.menuStyle" = "custom";
+    "window.titleBarStyle" = "native";
+    "workbench.editor.enablePreview" = false;
+  };
 }

53707e779e6294d059a095003e7de0be1754ed35
Juan C Nuno <juancnuno@gmail.com>
2025-10-19 19:47:03 -0700

    Simplify vscode.nix

--- vscode.nix
+++ vscode.nix
@@ -1,16 +1,18 @@
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
 
-  programs.vscode.profiles.default.userSettings = {
-    "editor.cursorBlinking" = "solid";
+    profiles.default.userSettings = {
+      "editor.cursorBlinking" = "solid";
 
-    "editor.rulers" = [
-      100
-    ];
+      "editor.rulers" = [
+        100
+      ];
 
-    "files.trimTrailingWhitespace" = true;
-    "window.menuStyle" = "custom";
-    "window.titleBarStyle" = "native";
-    "workbench.editor.enablePreview" = false;
+      "files.trimTrailingWhitespace" = true;
+      "window.menuStyle" = "custom";
+      "window.titleBarStyle" = "native";
+      "workbench.editor.enablePreview" = false;
+    };
   };
 }

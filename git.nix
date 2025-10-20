451a6c128423a89af9d5d337d493580815d130cd
Juan C Nuno <juancnuno@gmail.com>
2025-10-19 19:42:29 -0700

    Simplify git.nix

--- git.nix
+++ git.nix
@@ -1,38 +1,28 @@
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
 
-  programs.git.settings = {
-    credential = {
-      "https://gist.github.com/" = {
-        helper = [
+    settings = {
+      credential = {
+        "https://gist.github.com/".helper = [
+          ""
+          "!/run/current-system/sw/bin/gh auth git-credential"
+        ];
+
+        "https://github.com/".helper = [
           ""
           "!/run/current-system/sw/bin/gh auth git-credential"
         ];
       };
 
-      "https://github.com/" = {
-        helper = [
-          ""
-          "!/run/current-system/sw/bin/gh auth git-credential"
-        ];
+      diff.tool = "meld";
+      difftool.prompt = false;
+      init.defaultBranch = "main";
+
+      user = {
+        email = "juancnuno@gmail.com";
+        name = "Juan C Nuno";
       };
-    };
-
-    diff = {
-      tool = "meld";
-    };
-
-    difftool = {
-      prompt = false;
-    };
-
-    init = {
-      defaultBranch = "main";
-    };
-
-    user = {
-      email = "juancnuno@gmail.com";
-      name = "Juan C Nuno";
     };
   };
 }

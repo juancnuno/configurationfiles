0225b4d83d5f1589be1c93d595208536c582ba1c
Juan C Nuno <juancnuno@gmail.com>
2025-11-09 12:25:26 -0800

    Set 'spf'

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -2,5 +2,10 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
+
+    extraConfig = ''
+      set spf=/home/juancnuno/.config/vim/spell/en.utf-8.add
+      sil mksp! /home/juancnuno/.config/vim/spell/en.utf-8.add
+    '';
   };
 }
